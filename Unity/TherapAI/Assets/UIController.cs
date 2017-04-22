554e448df27e1873b75c6d73ba5c286929609986
Ryan Fuller <fullerryan42@gmail.com>
2017-04-22 17:55:01 +0100

    ui added to scene

---
+++ Unity/TherapAI/Assets/UIController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class UIController : MonoBehaviour {
+
+	public Text outtext;
+	public Text intext;
+
+	public void ReadText(){
+		//intext.text;
+		//use the above variable in the method that is used to get read text
+	}
+	public void WriteToScreen(string words){
+		outtext.text = words;
+	}
+
+}
