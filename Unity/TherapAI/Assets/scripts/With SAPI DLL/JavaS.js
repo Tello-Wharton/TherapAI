45348fe61150dc025148b11ffb389e9c49da8a4c
Zattri <daninjakidy@gmail.com>
2017-04-22 17:53:55 +0100

    Working alone like the single pringle I am - I added some comments to the scripts

--- Unity/TherapAI/Assets/scripts/With SAPI DLL/JavaS.js
+++ Unity/TherapAI/Assets/scripts/With SAPI DLL/JavaS.js
@@ -4,10 +4,12 @@
  
 function Update ()
 {
+	// When end key is pressed get the TestVoice 
     if (Input.GetKey(KeyCode.End))
     {
         var cs = GameObject.Find("TextVoice");
         var script = cs.GetComponent("TestVoice");
+        // Script object is the TestVoice script, and can then be used to call functions
         script.saySpeech("Hello world! How are you doing today?");
     }
     
